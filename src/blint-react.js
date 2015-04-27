17d74f88c4d0100e35356e618f139560b5d3e51b
gilbox <birmangil@gmail.com>
2015-04-26 21:24:09 -0500

    use invariant

--- src/blint-react.js
+++ src/blint-react.js
@@ -1,11 +1,88 @@
+// https://github.com/gilbox/blint
 if ("production" !== process.env.NODE_ENV) { // don't compile in production
 
   const React = require('react');
+  const invariant = require('react/lib/invariant');
   const MODULE = 0;
   const MODULE_MODIFIER = 1;
   const ELEMENT = 2;
   const ELEMENT_MODIFIER = 3;
   const createElement = React.createElement;
+
+  /**
+   * Try to detect and throw an error if you break css-bliss rules.
+   *
+   * @param className {string} original className passed into React.createElement
+   * @param elementType {string} used for logging error messages about an element
+   * @returns undefined
+   */
+  const lint = function(className, elementType) {
+    const classes = className.split(' ');
+    const types = [0, 0, 0, 0];
+    var moduleName;
+
+    classes.forEach(c => {
+      if (c.match(/^[A-Z]/)) {
+        if (moduleName) {
+          invariant(c.indexOf(moduleName) === 0,
+            `In ${elementType}[className="${className}"] there are classes from more than one CSS Module: ${c.split('-')[0]} and ${moduleName}. ` +
+            'A DOM element should not have more than one Module because this breaks encapsulation. ' +
+            'Use a wrapping DOM element instead to compose CSS Modules, or refactor and use a Modifier. ' +
+            'https://github.com/gilbox/css-bliss#encapsulation');
+        } else {
+          moduleName = c.split('-')[0];
+        }
+
+        // This regex matches all allowable Module classes
+        const matches = c.match(/^([A-Z]\w*)(\-([a-z]\w*))?(\-\-([a-z]\w*))?$/);
+
+        invariant(matches, `In ${elementType}[className="${className}"] the class ${c} starts with an uppercase letter, indicating ` +
+        'that it belongs to a module. However, it does not conform to the css-bliss naming ' +
+        'conventions for modules https://github.com/gilbox/css-bliss#naming');
+
+        const type = matches[5] && matches[3] ? ELEMENT_MODIFIER
+                                 : matches[5] ? MODULE_MODIFIER
+                                 : matches[3] ? ELEMENT
+                                 : MODULE;
+        types[type]++;
+      }
+    });
+
+    invariant(!(types[MODULE] && types[ELEMENT]),
+      `In ${elementType}[className="${className}"] there is a CSS Module and ` +
+      'an CSS Element. A DOM element ' +
+      'may have one or the other but not both. ' +
+      'https://github.com/gilbox/css-bliss/blob/master/common-mistakes.md#module-and-element-classes-applied-to-same-tag');
+
+    invariant(!(types[MODULE_MODIFIER] && types[ELEMENT]),
+      `In ${elementType}[className="${className}"] there is a CSS Module Modifier and an CSS Element. A DOM element ` +
+      'may have one or the other but not both.');
+
+    invariant(!(types[ELEMENT_MODIFIER] && types[MODULE]),
+      `In ${elementType}[className="${className}"] there is a CSS Module and an CSS Element Modifier. A DOM element ` +
+      'may have one or the other but not both.');
+
+    invariant(!(types[ELEMENT_MODIFIER] && types[MODULE_MODIFIER]),
+      `In ${elementType}[className="${className}"] there is a CSS Module Modifier ` +
+      'and an CSS Element Modifier. A DOM element ' +
+      'may have one or the other but not both.');
+
+    invariant(types[ELEMENT] <= 1,
+      `In ${elementType}[className="${className}"] there are multiple CSS Elements. ` +
+      'A DOM element may have at most one CSS Element.');
+
+    invariant(!(types[MODULE_MODIFIER] && !types[MODULE]),
+      `In ${elementType}[className="${className}"] there is a CSS Module Modifier, ` +
+      'but no CSS Module specified. If you are passing a Module Modifier into ' +
+      'another element that will always combine it with a CSS Module, use a custom prop like ' +
+      'classModifier instead');
+
+    invariant(!(types[ELEMENT_MODIFIER] && !types[ELEMENT]),
+      `In ${elementType}[className="${className}"] there is a CSS Element Modifier, ` +
+      'but no CSS Element specified. If you are passing an Element Modifier into ' +
+      'another element that will always combine it with a CSS Element, use a custom prop like ' +
+      'classModifier instead');
+  };
 
   // monkeypatch React
   React.createElement = function(el, opts) {
@@ -15,87 +92,4 @@ if ("production" !== process.env.NODE_ENV) { // don't compile in production
     }
     return createElement.apply(this, Array.prototype.slice.call(arguments));
   }
-
-  /**
-   * Try to detect and throw an arror if you break css-bliss rules.
-   *
-   * @returns {string}
-   */
-  function lint(className, elementType) {
-    const classes = className.split(' ');
-    const types = [0, 0, 0, 0];
-    var moduleName;
-
-    classes.forEach(c => {
-      if (c.match(/^[A-Z]/)) {
-        if (moduleName) {
-          if (!c.startsWith(moduleName)) {
-            const m = c.split('-')[0];
-            throw new Error(`In ${elementType}[className="${className}"] there is more than one CSS Module: ${m} and ${moduleName}. ` +
-            'A DOM element should not have more than one Module because this breaks encapsulation. ' +
-            'Use a wrapping DOM element instead to compose CSS Modules.' +
-            'https://github.com/gilbox/css-bliss#encapsulation');
-          }
-        } else {
-          moduleName = c.split('-')[0];
-        }
-
-        const matches = c.match(/^([A-Z]\w*)(\-([a-z]\w*))?(\-\-([a-z]\w*))?$/);
-        if (matches) {
-          const type = matches[5] && matches[3] ? ELEMENT_MODIFIER
-            : matches[5] ? MODULE_MODIFIER
-            : matches[3] ? ELEMENT
-            : MODULE;
-          types[type]++;
-        } else {
-          throw new Error(`In ${elementType}[className="${className}"] the class ${c} starts with an uppercase letter, indicating ` +
-          'that it belongs to a module. However, it does not conform to the css-bliss naming ' +
-          'conventions for modules https://github.com/gilbox/css-bliss#naming');
-        }
-      }
-    });
-
-    if (types[MODULE] && types[ELEMENT]) {
-      throw new Error(`In ${elementType}[className="${className}"] there is a CSS Module and ` +
-      'an CSS Element. A DOM element ' +
-      'may have one or the other but not both. ' +
-      'https://github.com/gilbox/css-bliss/blob/master/common-mistakes.md#module-and-element-classes-applied-to-same-tag');
-    }
-
-    if (types[MODULE_MODIFIER] && types[ELEMENT]) {
-      throw new Error(`In ${elementType}[className="${className}"] there is a CSS Module Modifier and an CSS Element. A DOM element ` +
-      'may have one or the other but not both.');
-    }
-
-    if (types[ELEMENT_MODIFIER] && types[MODULE]) {
-      throw new Error(`In ${elementType}[className="${className}"] there is a CSS Module and an CSS Element Modifier. A DOM element ` +
-      'may have one or the other but not both.');
-    }
-
-    if (types[ELEMENT_MODIFIER] && types[MODULE_MODIFIER]) {
-      throw new Error(`In ${elementType}[className="${className}"] there is a CSS Module Modifier ` +
-      'and an CSS Element Modifier. A DOM element ' +
-      'may have one or the other but not both.');
-    }
-
-    if (types[ELEMENT] > 1) {
-      throw new Error(`In ${elementType}[className="${className}"] there are multiple CSS Elements. ` +
-      'A DOM element may have at most one CSS Element.');
-    }
-
-    if (types[MODULE_MODIFIER] && !types[MODULE]) {
-      throw new Error(`In ${elementType}[className="${className}"] there is a CSS Module Modifier, ` +
-      'but no CSS Module specified. If you are passing a Module Modifier into ' +
-      'another element that will always combine it with a CSS Module, use a prop named ' +
-      'classModifier instead');
-    }
-
-    if (types[ELEMENT_MODIFIER] && !types[ELEMENT]) {
-      throw new Error(`In ${elementType}[className="${className}"] there is a CSS Element Modifier, ` +
-      'but no CSS Element specified. If you are passing an Element Modifier into ' +
-      'another element that will always combine it with a CSS Element, use a prop named ' +
-      'classModifier instead');
-    }
-  }
-
 }
